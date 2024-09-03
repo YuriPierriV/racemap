5939a793df1d13d9ef8fed6d36f569792b19a711
root <root@yuripierri>
2024-09-03 03:08:36 -0300

    add jsconfig e .env.development

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-
+import database from "infra/database.js"
 
 async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1;');
+  console.log(result);
   response.status(200).json({ status: "Status ok!" })
 }
 
