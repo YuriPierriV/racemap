24bbd44e60ab06c1f9dbb3f711a063b5bce2571a
Yuri Pierri <yuripierriv@gmail.com>
2024-09-20 02:11:10 -0300

    add mqttConfig.js

---
+++ infra/mqttConfig.js
@@ -0,0 +1,22 @@
+// mqttConfig.js
+const mqttConfig = {
+  host: process.env.NEXT_PUBLIC_HIVEMQ_HOST,
+  port: process.env.NEXT_PUBLIC_HIVEMQ_PORT,
+  protocol: 'wss',
+  topic: 'kart',
+  qos: 2,
+  clientId: `mqtt_${Math.random().toString(16).slice(3)}`,
+  username: process.env.NEXT_PUBLIC_HIVEMQ_USERNAME,
+  password: process.env.NEXT_PUBLIC_HIVEMQ_PASSWORD,
+};
+
+const connectUrl = `${mqttConfig.protocol}://${mqttConfig.host}:${mqttConfig.port}/mqtt`;
+
+const options = {
+  clientId: mqttConfig.clientId,
+  clean: true,
+  username: mqttConfig.username,
+  password: mqttConfig.password,
+};
+
+export { mqttConfig, connectUrl, options };
