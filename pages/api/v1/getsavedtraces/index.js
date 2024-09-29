d992c7f14450fd506b0d5eb6b636b04b17e7a9a1
Yuri Pierri <yuripierriv@gmail.com>
2024-09-29 02:09:10 -0300

    resposive map

--- pages/api/v1/getsavedtraces/index.js
+++ pages/api/v1/getsavedtraces/index.js
@@ -3,11 +3,7 @@ import database from "infra/database.js";
 async function getSavedTraces(request, response) {
   try {
     const result = await database.query('SELECT * FROM tracks ORDER BY created_at DESC');
-    if (result.rows.length > 0) {
-      response.status(200).json(result.rows);
-    } else {
-      response.status(404).json({ message: 'No trace found' });
-    }
+    response.status(200).json(result.rows);
   } catch (err) {
     console.error('Error fetching trace:', err);
     response.status(500).json({ error: 'Error fetching trace' });
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import mqtt from 'mqtt';
 import React, { useEffect, useState, useRef } from 'react';
 import { connectUrl, options } from 'infra/mqttConfig.js';
+import { haversineDistance } from './constants/functions';
+
 
 const MqttPage = () => {
   const [messages, setMessages] = useState([]);
@@ -18,9 +20,16 @@ const MqttPage = () => {
   const [savedTraces, setSavedTraces] = useState([]);
   const [selectedTrace, setSelectedTrace] = useState(null); // Traçado selecionado
   const [fiveTraces, setFiveTraces] = useState([]);
+  const [status, setStatus] = useState("aguardando");
+  const [widthCanvas, setWidthCanvas] = useState(0);
+  const [heightCanvas, setHeightCanvas] = useState(0);
 
   useEffect(() => {
     const mqttClient = mqtt.connect(connectUrl, options);
+    updateCanvasDimensions();
+
+    window.addEventListener('resize', updateCanvasDimensions);
+
     setClient(mqttClient);
 
     mqttClient.on('connect', () => {
@@ -74,7 +83,7 @@ const MqttPage = () => {
               return updatedTraces; // Retorna a lista atualizada
             }
           } else {
-            // Caso a lista de traçados anteriores já tenha 5 posições, adiciona a nova posição e remove a mais antiga
+            // Caso a lista de traçados anteriores já tenha 5 posições, adiciona a nova posição
             updatedTraces = [position, ...prevFive]; // Mantém apenas as 5 posições mais recentes
 
             // Calcula a média das latitudes e longitudes das 5 últimas posições
@@ -99,7 +108,7 @@ const MqttPage = () => {
 
                 // Se a distância for maior que 2 metros e menor que 4 metros, adiciona a média à lista de traçados
                 if (distance > 2) {
-                  console.log(distance); // Exibe a distância no console
+
 
                   // Atualiza os valores mínimos e máximos de latitude
                   setLatMin(prevMin => Math.min(prevMin, avgPosition.lat));
@@ -111,7 +120,7 @@ const MqttPage = () => {
 
                   return [avgPosition, ...prevTrace]; // Adiciona a média ao início da lista de 'trace'
                 }
-                console.log(distance + " naoo passou");
+
                 return prevTrace;
               } else {
                 // Se 'trace' estiver vazio, adiciona a nova posição média como o primeiro item
@@ -145,6 +154,7 @@ const MqttPage = () => {
 
     return () => {
       mqttClient.end();
+      window.removeEventListener('resize', updateCanvasDimensions);
     };
   }, [mode]);
 
@@ -175,7 +185,7 @@ const MqttPage = () => {
 
       if (traceToDraw.length > 0) {
 
-        drawTrace(traceToDraw); // Desenha o traçado
+        drawTrace(traceToDraw, widthCanvas, heightCanvas); // Desenha o traçado
       }
     };
 
@@ -190,30 +200,22 @@ const MqttPage = () => {
       const newScaleY = canvasRef.current.height / (latMax - latMin);
 
       setScale({ scaleX: newScaleX, scaleY: newScaleY });
-      drawTrace(trace);
+      drawTrace(trace, widthCanvas, heightCanvas);
     }
 
   }, [trace]); // Dependências que devem acionar a atualização
 
 
-  const haversineDistance = (lat1, long1, lat2, long2) => {
-    const R = 6371000; // Raio da Terra em metros
-    const toRadians = (degrees) => degrees * (Math.PI / 180);
-    const dLat = toRadians(lat2 - lat1);
-    const dLong = toRadians(long2 - long1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
-      Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c; // Distância em metros
-  };
 
-  const drawTrace = (positions) => {
+
+  const drawTrace = (positions, width, height) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    ctx.clearRect(0, 0, width, height);
+
+    console.log(height);
+    console.log(width);
     if (positions.length === 0) return;
 
     if (positions.length > 3) {
@@ -223,15 +225,15 @@ const MqttPage = () => {
     const padding = 50; // Define o padding de 20px
 
     // Calcula novas escalas considerando o padding
-    const adjustedScaleX = (canvas.width - 2 * padding) / (longMax - longMin);
-    const adjustedScaleY = (canvas.height - 2 * padding) / (latMax - latMin);
+    const adjustedScaleX = (width - 2 * padding) / (longMax - longMin);
+    const adjustedScaleY = (height - 2 * padding) / (latMax - latMin);
 
     ctx.beginPath();
     positions.forEach((pos, index) => {
+
       // Ajusta as coordenadas com o padding
       const x = (pos.long - longMin) * adjustedScaleX + padding;
-      const y = canvas.height - ((pos.lat - latMin) * adjustedScaleY + padding); // O canvas inverte o eixo Y, então usamos canvas.height
-
+      const y = height - ((pos.lat - latMin) * adjustedScaleY + padding); // O canvas inverte o eixo Y, então usamos height
       if (index === 0) {
         ctx.moveTo(x, y);
       } else {
@@ -247,13 +249,20 @@ const MqttPage = () => {
     positions.forEach((pos) => {
       // Ajusta as coordenadas dos pontos com o padding
       const x = (pos.long - longMin) * adjustedScaleX + padding;
-      const y = canvas.height - ((pos.lat - latMin) * adjustedScaleY + padding);
+      const y = height - ((pos.lat - latMin) * adjustedScaleY + padding);
 
       ctx.beginPath();
       ctx.arc(x, y, 2, 0, Math.PI * 2);
       ctx.fillStyle = 'red';
       ctx.fill();
     });
+  };
+
+  const updateCanvasDimensions = () => {
+    if (canvasRef.current) {
+      setWidthCanvas(canvasRef.current.offsetWidth);
+      setHeightCanvas(canvasRef.current.offsetHeight);
+    }
   };
 
 
@@ -270,6 +279,9 @@ const MqttPage = () => {
   };
 
   const createTrace = () => {
+    setStatus("iniciado")
+    setMode("Criando traçado");
+    /*
     if (client) {
       setMode("Criando traçado");
       client.publish('config', "10", { qos: 2 }, (err) => {
@@ -278,6 +290,7 @@ const MqttPage = () => {
         }
       });
     }
+    */
   };
 
   const saveTrace = () => {
@@ -357,40 +370,125 @@ const MqttPage = () => {
             </div>
           </div>
 
-          <h2 className="text-2xl font-bold mb-4">Histórico:</h2>
+          {mode === 'Criando traçado' ? (
+            <div>
+              <form>
+                <div className="space-y-12">
+                  <div className="border-b border-gray-900/10 pb-12">
+                    <div className="mt-5  grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                      <div className="sm:col-span-2">
+                        <h2 className="text-base font-semibold leading-7 text-white">Inicie um traçado</h2>
+                        <p className="mt-1 text-sm leading-6 text-slate-400">Tenha um GPS conectado</p>
+                      </div>
+                      <div className="sm:col-span-4">
 
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Device ID
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Latitude
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Longitude
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {messages.map((message, index) => (
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                    <th scope="row" className="md:px-6 px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                      {message.deviceId}
-                    </th>
-                    <td className="md:px-6 px-2 py-4">
-                      {message.lat}
-                    </td>
-                    <td className="md:px-6 px-2 py-4">
-                      {message.long}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                        <ol className="flex justify-center items-center w-full p-3 space-x-2 text-sm font-medium text-center text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700 sm:p-4 sm:space-x-4 rtl:space-x-reverse">
+                          <li className="flex items-center text-blue-600 dark:text-blue-500">
+                            <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
+                              1
+                            </span>
+                            Externo
+                            <svg className="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
+                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m7 9 4-4-4-4M1 9l4-4-4-4" />
+                            </svg>
+                          </li>
+                          <li className="flex items-center">
+                            <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+                              2
+                            </span>
+                            Interno
+                            <svg className="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
+                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m7 9 4-4-4-4M1 9l4-4-4-4" />
+                            </svg>
+                          </li>
+                          <li className="flex items-center">
+                            <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+                              3
+                            </span>
+                            Ajustes
+                          </li>
+                        </ol>
+
+
+                      </div>
+
+                    </div>
+
+
+                    <div className="mt-5  grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 dark:bg-gray-800 dark:border-gray-700 sm:p-4 sm:space-x-4 rtl:space-x-reverse p-3 space-x-2 text-sm font-medium text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm">
+                      <div className="sm:col-span-full">
+                        <label className="block text-sm font-medium leading-6 text-white ">
+                          Nome do traçado
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            id="kartname"
+                            name="kartname"
+                            type="text"
+                            autoComplete="kartname"
+                            className="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-inset focus:ring-blue-800 sm:text-sm sm:leading-6"
+                          />
+                        </div>
+                      </div>
+                      <div className="mt-3 sm:col-span-full mx-10 flex items-center justify-end gap-x-6">
+                        <button type="button" className="text-sm font-semibold leading-6 text-gray-400">
+                          Cancelar
+                        </button>
+                        <button
+                          type="submit"
+                          className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                        >
+                          Iniciar
+                        </button>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+
+
+              </form>
+            </div>
+          ) : (
+            <div>
+              <h2 className="text-2xl font-bold mb-4">Histórico:</h2>
+
+              <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3">
+                        Device ID
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Latitude
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Longitude
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {messages.map((message, index) => (
+                      <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" className="md:px-6 px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {message.deviceId}
+                        </th>
+                        <td className="md:px-6 px-2 py-4">
+                          {message.lat}
+                        </td>
+                        <td className="md:px-6 px-2 py-4">
+                          {message.long}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}
+
+
         </div>
 
         <div>
@@ -417,42 +515,41 @@ const MqttPage = () => {
             >
               Criar traçado
             </button>
-            <button
-              onClick={saveTrace}
-              disabled={trace.length === 0}
-              className="px-4 py-2 bg-yellow-500 text-white rounded disabled:opacity-50 font-bold"
-            >
-              Salvar Traçado
-            </button>
+
           </div>
 
           <h3 className="text-xl font-semibold mb-2">Traces:</h3>
-          <ul role="list" class="divide-y divide-gray-100">
+          <ul role="list" className="divide-y divide-gray-100">
             {savedTraces.map((trace, index) => (
-              <li class="flex justify-between gap-x-6 py-5">
-                <div class="flex min-w-0 gap-x-4">
+              <li className="flex justify-between gap-x-6 py-5">
+                <div className="flex min-w-0 gap-x-4">
 
-                  <div class="min-w-0 flex-auto">
-                    <p class="text-sm font-semibold leading-6 text-gray-900">{trace.id}</p>
-                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">{trace.name}</p>
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{trace.id}</p>
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{trace.name}</p>
                   </div>
                 </div>
-                <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                  <p class="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
-                  <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">{trace.created_at}</time></p>
+                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                  <p className="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
+                  <p className="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">{trace.created_at}</time></p>
                 </div>
               </li>
 
             ))}
-
-
           </ul>
+
+
+
 
 
           <canvas
             ref={canvasRef}
-            className="border border-black h-auto w-full rounded dark:bg-gray-700"
+            className={`border border-black dark:bg-gray-700 h-auto w-full rounded`}
+            id="tracado"
           />
+
+
+
         </div>
       </div>
     </main>
