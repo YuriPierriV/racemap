945505117ee7ada4f7a0e8afdd95130940db52ed
Yuri Pierri <yuripierriv@gmail.com>
2024-10-07 22:19:17 -0300

    add /lista para implementação de listagem dos traçados

---
+++ pages/lista/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+export default function Lista() {
+  // Aqui você pode inicializar os estados e usar hooks como useEffect e useRef, se necessário.
+
+  return (
+    <div>
+      Oi
+    </div>
+  );
+}
