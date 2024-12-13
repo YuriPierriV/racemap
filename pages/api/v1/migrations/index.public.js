b06bd484ec5f8c59fde689889fd015a1d67acd41
Yuri Pierri <yuripierriv@gmail.com>
2024-12-13 07:26:40 -0300

    dbClient

--- pages/api/v1/migrations/index.public.js
+++ pages/api/v1/migrations/index.public.js
@@ -1,22 +1,24 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      dbClient: dbClient,
       dryRun: true,
       dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
     });
-
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
   if (request.method === "POST") {
     const migratedMigrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      dbClient: dbClient,
       dryRun: false,
       dir: join("infra", "migrations"),
       direction: "up",
@@ -24,6 +26,7 @@ export default async function migrations(request, response) {
       migrationsTable: "pgmigrations",
     });
 
+    await dbClient.end();
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
