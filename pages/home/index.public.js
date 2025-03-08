f96f7d9e357335cffef456b2e4c02522b0febbdf
YuriPierriV <yuripierriv@gmail.com>
2025-03-08 17:16:59 -0300

    feat: adiciona pagina home

--- pages/home/index.public.js
+++ pages/home/index.public.js
@@ -1,24 +1,26 @@
-import { Layout } from "pages/components/app-sidebar";
-import Header from "pages/components/Header";
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import LayoutMainPainel from "pages/components/main-painel";
 
 
-
-export async function getServerSideProps(context) {
-    const cookies = context.req.cookies || {}; // Pega os cookies do request
-    const defaultOpen = cookies["sidebar_state"] === "true";
-
-    return {
-        props: { defaultOpen },
-    };
-}
 
 export default function Home({ defaultOpen }) {
 
 
     return (
-        <Layout defaultOpen={defaultOpen}>
-            <Header></Header>
-
-        </Layout>
+        <LayoutMainPainel>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Home</CardTitle>
+                    <CardDescription>Descrição da home</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Home</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Home Footer</p>
+                </CardFooter>
+            </Card>
+        </LayoutMainPainel>
     )
 }
