c5a0c827f2117b320baab945a2bd3fc28dc4d37a
YuriPierriV <yuripierriv@gmail.com>
2025-03-08 17:17:51 -0300

    feat: adiciona tela pilotos

---
+++ pages/pilotos/index.public.js
@@ -0,0 +1,26 @@
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import LayoutMainPainel from "pages/components/main-painel";
+
+
+
+export default function Pilotos({ defaultOpen }) {
+
+
+    return (
+        <LayoutMainPainel>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Pilotos</CardTitle>
+                    <CardDescription>Descrição da Pilotos</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Pilotos</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Pilotos Footer</p>
+                </CardFooter>
+            </Card>
+        </LayoutMainPainel>
+    )
+}
