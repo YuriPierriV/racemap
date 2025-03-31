6697b5e3235de52b29b7f551c34ed054d51475ee
YuriPierriV <yuripierriv@gmail.com>
2025-03-30 21:09:45 -0300

    fix: utiliza o setSSLalues

--- infra/database.js
+++ infra/database.js
@@ -27,7 +27,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   await client.connect();
@@ -38,3 +38,13 @@ export default {
   query,
   getNewClient,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
