14d430a478b1fb2a2f4bc52e22701e3f744b1aea
YuriPierriV <yuripierriv@gmail.com>
2025-03-30 23:26:30 -0300

    feat: a
    a

--- infra/database.js
+++ infra/database.js
@@ -13,11 +13,14 @@ async function query(queryObject) {
     const ServiceErrorObject = new ServiceError({
       message: "Erro na conexão com o Banco de dados ou na Query",
       cause: {
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      ssl: process.env.NODE_ENV !== "development",
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+        ssl: process.env.NODE_ENV === "development" ? false : {
+          require: true,
+        },
       },
     });
     throw ServiceErrorObject;
