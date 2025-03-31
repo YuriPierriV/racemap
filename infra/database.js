203087625808c6fe09d346396714ae87fddc147c
YuriPierriV <yuripierriv@gmail.com>
2025-03-30 23:39:37 -0300

    feat(a): a

--- infra/database.js
+++ infra/database.js
@@ -32,7 +32,7 @@ async function query(queryObject) {
 async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: parseInt(process.env.POSTGRES_PORT),
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
